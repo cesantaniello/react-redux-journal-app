90084dae4067ad81057db1ca68f481f0af797322
Administrator <admin@example.com>
2022-01-28 18:59:15 +0100

    Crear dispatch de accion a Store

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm';
+import { login } from '../../actions/auth';
 
 export const LoginScreen = () => {
+    const dispatch = useDispatch();
+
+    const [formValues, handleInputChange] = useForm({
+        email: 'carlos@gmail.com',
+        password: '12345'
+    });
+
+    const { email, password } = formValues;
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        dispatch(login(12345, 'Carlos'));
+    }
+
     return (
         <>
             <h3 className="auth__title">Login</h3>
 
-            <form>
+            <form onSubmit={handleLogin}>
 
                 <input 
                     type="text"
@@ -14,6 +31,8 @@ export const LoginScreen = () => {
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
                 />
 
                 <input 
@@ -21,6 +40,8 @@ export const LoginScreen = () => {
                     placeholder="Password"
                     name="password"
                     className="auth__input"
+                    value={password}
+                    onChange={handleInputChange}                    
                 />
 
 
