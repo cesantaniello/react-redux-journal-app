de9eba962f3a8941a02031ae74e75413565d3255
Administrator <admin@example.com>
2022-01-29 18:13:29 +0100

    Crea acciones asincronas con Thunk Middleware

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,13 @@
 import { types } from "../types/types"
 
+export const startLoginEmailPassword = (email, password) => {
+    return (dispatch) => {
+        setTimeout(() => {
+            dispatch(login(123, 'Carlos'));
+        }, 2000);
+    }
+}
+
 export const login = (uid, displayName) => ({
     type: types.login,
     payload: {
