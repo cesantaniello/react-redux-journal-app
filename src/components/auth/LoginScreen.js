6cf3a73e1106d7153aaabcf2d0a561c62bc9a155
Administrator <admin@example.com>
2022-02-02 18:13:35 +0100

    Crea Loading state

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm';
 import { startLoginEmailPassword, startGoogleLogin } from '../../actions/auth';
 
 export const LoginScreen = () => {
     const dispatch = useDispatch();
+    const {loading} = useSelector(state => state.ui);
 
     const [formValues, handleInputChange] = useForm({
         email: 'carlos@gmail.com',
-        password: '12345'
+        password: '123456'
     });
 
     const { email, password } = formValues;
@@ -52,6 +53,7 @@ export const LoginScreen = () => {
                 <button
                     type="submit"
                     className="btn btn-primary btn-block"
+                    disabled={loading}
                 >
                     Login
                 </button>
