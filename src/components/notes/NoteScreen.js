d69e3f80a92fdc8c6e1b76cdf7491c2cf668ea2a
Administrator <admin@example.com>
2022-02-09 13:34:14 +0100

    Actualiza nota actual

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useRef } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useRef} from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+
+    const dispatch = useDispatch();
 
     const {active:note} = useSelector( state => state.notes );
     const [formValues, handleInputChange, reset] = useForm( note );
@@ -20,6 +21,10 @@ export const NoteScreen = () => {
         }
     }, [note, reset])
     
+    useEffect(() => {
+        dispatch(activeNote(formValues.id, {...formValues}));
+    }, [formValues, dispatch]);
+    
 
     return (
         <div className="notes__main-content">
@@ -33,6 +38,7 @@ export const NoteScreen = () => {
                     placeholder="Some awesome title"
                     className="notes__title-input"
                     autoComplete="off"
+                    name="title"
                     value={title}
                     onChange={handleInputChange}
                 />
@@ -40,6 +46,7 @@ export const NoteScreen = () => {
                 <textarea
                     placeholder="What happened today"
                     className="notes__textarea"
+                    name="body"
                     value={body}
                     onChange={handleInputChange}
                 ></textarea>
