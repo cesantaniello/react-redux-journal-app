c77f553560e16053e4f7cfff54f65116cae3c974
Administrator <admin@example.com>
2022-02-09 17:47:52 +0100

    Sube imagen a servidor cloud

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -3,6 +3,7 @@ import { loadNotes } from '../helpers/loadNotes';
 import { types } from '../types/types';
 
 import Swal from 'sweetalert2';
+import { fileUpload } from '../helpers/fileUpload';
 
 export const startNewNote = () => {
     return async(dispatch, getState) => {
@@ -68,3 +69,12 @@ export const refreshNote = (id, note) => ({
         }
     }
 })
+
+export const startUploading = (file) => {
+    return async(dispatch, getState) => {
+        const {active:activeNote} = getState().notes;
+        const fileUrl = await fileUpload(file);
+
+        console.log(fileUrl);
+    }
+}
