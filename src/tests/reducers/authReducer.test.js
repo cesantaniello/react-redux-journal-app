772c95b1c6e052071ecce08b3946392c34ec099e
Administrator <admin@example.com>
2022-02-11 18:08:19 +0100

    Pruebas en authReducer

---
+++ src/tests/reducers/authReducer.test.js
@@ -0,0 +1,37 @@
+import { authReducer } from "../../reducers/authReducer";
+import { types } from "../../types/types";
+
+describe('Tests on authReducer', () => {
+    test('should do login', () => {
+        const initState = {};
+        const action = {
+            type: types.login,
+            payload: {
+                uid: '123',
+                displayName: 'Juan'
+            }
+        };
+
+        const state = authReducer(initState, action);
+        
+        expect(state).toEqual({
+            uid: '123',
+            name: 'Juan'
+        })
+    })
+
+    test('should not make changes on the state', () => {
+        const initState = {                
+            uid: '123',
+            displayName: 'Juan'
+        };
+
+        const action = {
+            type: 'aasdasdasd',
+        };
+
+        const state = authReducer(initState, action);
+        
+        expect(state).toEqual(initState);
+    })    
+})
