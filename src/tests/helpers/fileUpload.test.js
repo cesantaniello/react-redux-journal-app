5de0664bb79d73c61e07c4167a5ac70c298239c3
Administrator <admin@example.com>
2022-02-11 17:12:52 +0100

    Pruebas de eliminar imagenes en el servidor cloud

--- src/tests/helpers/fileUpload.test.js
+++ src/tests/helpers/fileUpload.test.js
@@ -1,7 +1,15 @@
+import cloudinary from 'cloudinary';
 import { fileUpload } from "../../helpers/fileUpload";
 
+cloudinary.config({ 
+    cloud_name: 'ddvyehc8b', 
+    api_key: '978343733914881', 
+    api_secret: 'mJHkLxigk8xVRTm_Qk8qj2ov9v0',
+    secure: true
+});
+
 describe('Tests on fileUpload', () => {
-    test('Should loading a file and return an URL', async () => {
+    test('Should load a file and return an URL', async (done) => {
         const resp = await fetch('https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg');
         const blob = await resp.blob();
 
@@ -9,6 +17,13 @@ describe('Tests on fileUpload', () => {
         const url = await fileUpload(file);
 
         expect(typeof url).toBe('string');
+
+        const segments = url.split('/');
+        const imageId = segments[segments.length-1].replace('.jpg','');
+
+        cloudinary.v2.api.delete_resources(imageId, {}, () => {
+            done();
+        });
     })
 
     test('Should return an error', async () => {
