3371f806ff9af2c98a2e5122c358e4a928406b2a
Administrator <admin@example.com>
2022-02-11 11:19:30 +0100

    Prueba sobre types

---
+++ src/tests/types/types.test.js
@@ -0,0 +1,24 @@
+import { types } from "../../types/types"
+
+describe('Tests on types', () => {
+    test('Should be these types', () => {
+        expect(types).toEqual({
+            login: '[Auth] Login',
+            logout: '[Auth] Logout',
+
+            uiSetError: '[UI] Set Error',
+            uiRemoveError: '[UI] Remove Error',
+
+            uiStartLoading: '[UI] Start loading',
+            uiFinishLoading: '[UI] Finish loading',
+        
+            notesAddNew: '[Notes] New note',
+            notesActive: '[Notes] Set active note',
+            notesLoad: '[Notes] Load notes',
+            notesUpdated: '[Notes] Updated note',
+            notesFilesUrl: '[Notes] Updated image url',
+            notesDelete: '[Notes] Delete note',
+            notesLogoutCleaning: '[Notes] Logout Cleaning',
+        })
+    })
+})
